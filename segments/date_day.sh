b3b1b3febafc8e07f8247e251728768565cca0ab
tohta <tohta@valuecommerce.com>
2015-10-19 15:21:37 +0900

    my config

--- segments/date_day.sh
+++ segments/date_day.sh
@@ -1,5 +1,5 @@
 # Prints the name of the current day.
-
+LANG=C
 run_segment() {
 	date +"%a"
 	return 0
--- themes/default.sh
+++ themes/default.sh
@@ -1,10 +1,10 @@
 # Default Theme
 
 if patched_font_in_use; then
-	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD=""
-	TMUX_POWERLINE_SEPARATOR_LEFT_THIN=""
-	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD=""
-	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN=""
+	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="⮂"
+	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="⮃"
+	TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD="⮀"
+	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="⮁"
 else
 	TMUX_POWERLINE_SEPARATOR_LEFT_BOLD="◀"
 	TMUX_POWERLINE_SEPARATOR_LEFT_THIN="❮"
@@ -24,35 +24,48 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		#"hostname 33 255" \
+		#"hostname 106 234 ${TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}" \
+		"hostname 1 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
-		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
-		"vcs_others 245 0" \
+		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		"wan_ip 26 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_BOLD}" \
+		#"wan_ip 33 255" \
+		"wan_ip 33 255" \
+		#"lan_ip 33 255" \
+		#"vcs_branch 29 88" \
+		#"vcs_compare 60 255" \
+		#"vcs_staged 64 255" \
+		#"vcs_modified 9 255" \
+		#"vcs_others 245 0" \
 	)
 fi
 
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
-		"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"pwd 89 211" \
+		#"mailcount 9 255" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
-		"load 237 167" \
+		#"load 33 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
+		#"load 6 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
+		"load 33 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
-		"weather 37 255" \
+		#"battery 137 127" \
+		#"battery 53 6 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOLD}"\
+		#"battery 5 6" \
+		#"weather 37 255" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"date_day 235 136" \
+		#"date 1 6 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOD}" \
+		"date 5 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_BOD}" \
+		#"date_day 1 6 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date_day 5 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"time 1 6 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"time 5 255 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
